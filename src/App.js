7228a52726db1965e8a26e1648830709b1a24dbb
WesleyDev <57929638+WesleyDev184@users.noreply.github.com>
2022-09-08 14:48:25 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import "./App.scss";
+import { Notification, Section } from 'rbx';
 
-const App = () => <p>Hello World</p>
+const App = () => (
+    <div>
+        <Section>
+            <Notification color="success">
+                Javascript Notes!
+            </Notification>
+        </Section>
+    </div>
+)
 
 export default App;
